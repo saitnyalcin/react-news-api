cfbe4d725edb1e6c1edb7c3cdbab2de7ec62c7c5
Steve Yalcin <steve.yalcin@ibm.com>
2021-03-06 15:09:40 -0500

    axios lib is removed and code base modification

--- src/widgets/News.js
+++ src/widgets/News.js
@@ -1,18 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import '../style/style.scss';
+import React, { useEffect, useState } from "react";
+import "../style/style.scss";
 
 const News = () => {
   let todayDate = new Date(); // set variable to today's date to fetch today's news
 
   const [data, setData] = useState({ articles: [] });
+
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios(
+      await fetch(
         `http://newsapi.org/v2/everything?q=bitcoin&from=${todayDate}&sortBy=publishedAt&apiKey=080166ce3b2842bba7e9d29cc29840c4`
-      );
-      setData(result.data);
+      )
+        .then((response) => response.json())
+        .then((data) => setData(data))
+        .then((result) => {
+          console.log("Success:", result);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
     };
+
     fetchData();
   }, [todayDate]);
   return (
@@ -20,7 +28,7 @@ const News = () => {
       <h2>Today's News</h2>
       <ul>
         {data.articles.map((news, index) => (
-          <li key={index} style={{ listStyle: 'none' }}>
+          <li key={index} style={{ listStyle: "none" }}>
             <div className="container">
               <h5>
                 <span role="img" aria-label="calendar">
